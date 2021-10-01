ab23f94e7f84222bdcefde0d3ed7b9151f1009ae
Rhian Cavalcanti <rhiancavalcanti10@hotmail.com>
2021-10-01 14:51:41 -0300

    Criação das Classes Heroi, Monstro e Mapa

--- Jogo/Config.cs
+++ Jogo/Config.cs
@@ -1,35 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Jogo
 {
+
+
+    class Heroi
+    {
+        string valor = "H";
+        int vida = 25;
+        int dano = 1;
+
+    }
+    class Monstro
+    {
+        string valor = "M";
+        int vida = 5;
+        int dano = 1;
+    }
+
+    class Mapa
+    {
+        public static string[,] mapa = new string[20, 20]
+            {
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"},
+                {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"}
+            };
+
+    }
+
+
+
+
+
+
     class Config
     {
-        struct Entidades
-        {
-            public string valor;
-            public int vida;
-            public int dano;
-            
 
-            public Entidades(string valor, int vida, int dano, int score)
-            {
-                this.valor = valor;
-                this.vida = vida;
-                this.dano = dano;
-             
-
-            }
-
-
-        }
-        
         public static string MovimentoHorizontalMonstros()
         {
             Random rnd = new Random();
             string direcao;
-            string[] sentidos = {"direita","esquerda" };
+            string[] sentidos = { "direita", "esquerda" };
             int indice = rnd.Next(sentidos.Length);
             direcao = sentidos[indice];
 
@@ -47,7 +79,21 @@ namespace Jogo
             return direcao;
         }
 
+        public static bool NascimentoMonstros()
+        {
+            Random rnd = new Random();
 
+            int[] horizontal = { 2, 17 };
+            int[] vertical = { 2, 17 };
+            int indicehorizontal = rnd.Next(horizontal.Length);
+            int indicevertical = rnd.Next(vertical.Length);
+            int posicaohorizontal = horizontal[indicehorizontal];
+            int posicaovertical = vertical[indicevertical];
+
+
+
+            return true;
+        }
 
     }
 }
