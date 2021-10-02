abd545cea2dbb8bd25ac3d838f81a2a0e0724663
Nilo Sandes <Nilo Sandes@DESKTOP-R3MH7RQ>
2021-10-02 01:58:19 -0300

    Encapsulamento dos atributos e criação de métodos de acesso.

--- Jogo/Personagens.cs
+++ Jogo/Personagens.cs
@@ -4,22 +4,45 @@ using System.Text;
 
 namespace Jogo
 {
-    class Heroi
+    public class Heroi
     {
 
-        public static string valor = "H";
-        public int vida = 25;
-        public int dano = 1;
+        private static string valor = "H";
+        private static int vida = 25;
+        private int dano = 1;
 
+        public String getValor()
+        {
+            return valor;
+        }
+        
+        public void reduzVida()
+        {
+            vida--;
+        }
 
+        public void pegaArma()
+        {
+            dano++;
+        }
 
     }
+    
     class Monstro
     {
-        public static string valor = "M";
-        public int vida = 5;
-        public int dano = 1;
+        private static string valor = "M";
+        private int vida = 5;
+        private int dano = 1;
 
+        public static String getValor()
+        {
+            return valor;
+        }
+
+        public void reduzVida()
+        {
+            vida--;
+        }
     }
 
 
